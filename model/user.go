33847648a9c73435ec041d30a8b335916a0c6703
gallifrey <956465331@qq.com>
2023-05-21 19:19:45 +0800

    feat: 添加用户模型

---
+++ model/user.go
@@ -0,0 +1,12 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Email        string
+	Sms          string
+	Nickname     string
+	Avatar       string
+	Introduction string
+}
